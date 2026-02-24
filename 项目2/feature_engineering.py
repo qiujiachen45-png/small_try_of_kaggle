bfce2cec9392368aa94bd5ed1a243682ea38a663
qiujiachen45-png <qiujiachen45@gmail.com>
2026-02-24 18:42:53 +0800

    Update feature_engineering.py

--- 项目2/feature_engineering.py
+++ 项目2/feature_engineering.py
@@ -1,3 +1,4 @@
+
 from hardship1 import db_train,db_test
 from sklearn.feature_selection import mutual_info_regression
 import pandas as pd
@@ -9,7 +10,7 @@ X=db_train
 
 
 X=data_cleaning(X)
-
+#此函数的意义是通过互相关检验因变量和自变量的相关性，筛选出前五个特征
 def mutual_relationship(X1,y):
 
     mi_score11=mutual_info_regression(X1,y,discrete_features=False)
