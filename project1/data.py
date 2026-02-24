f75450ab3b2f1102788cda2195dea039e971604a
qiujiachen45-png <qiujiachen45@gmail.com>
2026-02-24 19:49:22 +0800

    Update data.py

--- project1/data.py
+++ project1/data.py
@@ -19,7 +19,8 @@ from sklearn.decomposition import PCA
 df=pd.read_csv("../datasets/train.csv")
 num=df.select_dtypes(include=np.number).columns.tolist()
 word=df.select_dtypes(include=["object","category"]).columns.tolist()
-
+#数据清理管道
+#num清理，word的编码
 num_transform=Pipeline([("Imputer",SimpleImputer(strategy="median")),("scaler",StandardScaler())])
 word_transform=Pipeline([("imputer",SimpleImputer(strategy="constant",fill_value="missing")),("onehot",OneHotEncoder(handle_unknown="ignore",sparse_output=False))])
 
@@ -28,13 +29,13 @@ df[word]=word_transform.fit_transform(df[word])
 
 X=df.copy()
 y=df.pop("SalePrice")
-
+#防止数据渗漏
 for i in X.select_dtypes("object"):
     X[i],_=X[i].factorize()
 
 discrete_features=X.dtypes==int
 
-#互信息
+#互信息筛选前十变量
 def score_plot(X,y,discrete_features):
    mi_score=mutual_info_regression(X,y,discrete_features=discrete_features)
    mi_score=pd.DataFrame(mi_score,columns=["scores"],index=X.columns)
@@ -48,7 +49,7 @@ the_chosen1=df[the_top_feature].select_dtypes(include=[np.number]).columns.tolis
 the_chosen1=df[the_chosen1]
 scaler=StandardScaler()
 X_pca_scaled=scaler.fit_transform(the_chosen1)
-
+降维为三维
 pca=PCA(n_components=3)
 X_pca=pca.fit_transform(X_pca_scaled)
 X_pca_df=pd.DataFrame(X_pca,columns=["PC1","PC2"],index=df.index)
