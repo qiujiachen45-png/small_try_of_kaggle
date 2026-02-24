acfa6dbea9afe1a241e450be3658454b71c36e9a
qiujiachen45-png <qiujiachen45@gmail.com>
2026-02-24 20:23:24 +0800

    Delete datasets1/map1.py

--- datasets1/map1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-df=pd.read_csv("train.csv")
-sss=df.select_dtypes(include=["object"]).columns.tolist()
-print(f"{sss}")
